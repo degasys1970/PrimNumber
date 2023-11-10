06cc51860463075b3817a94e67551892fefeb8ef
degasys1970 <150461049+degasys1970@users.noreply.github.com>
2023-11-10 19:30:47 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+def is_prim(num):
+    for i in range(2,num):
+      if num % i == 0:
+        return False
+    return True
+  
+num = int(input("Enter a number: "))
+if is_prim(num):
+  print(num, "is a prime number")
+else:
+  print(num, "is not a prime number")
